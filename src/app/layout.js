4f5f49cc27442ba391db597518899f1d54f914ee
Atikur <atikurrahman1587@gmailcom>
2023-10-23 20:46:01 +0600

    Create The Header Section

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Header from './components/Header'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -13,9 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-      <link rel="icon" href="/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico"/>
       </head>
-      <body>{children}</body>
+      <body>
+        {/* Header */}
+        <Header/>
+        {/* Navbar */}
+
+        {/* SearchBox */}
+        {children}
+      </body>
     </html>
   )
 }
